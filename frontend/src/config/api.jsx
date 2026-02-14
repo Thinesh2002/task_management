10868f662a44f3bae8f31a30edb21de2dd0925b6
Thineshkaran <sthinesh0730@gmail.com>
2026-02-14 20:30:51 +0530

    1

--- frontend/src/config/api.jsx
+++ frontend/src/config/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://api.tasks.teckvora.com/api"
 });
 
 API.interceptors.request.use(cfg => {
